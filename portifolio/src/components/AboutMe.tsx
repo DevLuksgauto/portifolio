8f63cfff8632fecc67abf47b34dc39ae49452f23
Lucas Gauto <lukscgauto@gmail.com>
2023-10-05 18:58:44 -0300

    Project update: skills page done

--- portifolio/src/components/AboutMe.tsx
+++ portifolio/src/components/AboutMe.tsx
@@ -1,19 +1,16 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import classes from '../css/AboutMe.module.css';
 import { RootState } from '../reducers';
-import { useEffect } from 'react';
-import { fetchData } from '../action/fetchActions';
 import separator from '../assets/separatorBlack 1.png'
+
+import developmentPic from '../assets/development.png';
+import maintenancePic from '../assets/maintenance.png';
+import communicationPic from '../assets/communication.png';
 
 const AboutMe: React.FC = () => {
 
-    const dispatch = useDispatch();
     const data = useSelector((state: RootState) => state.data.data);
     const lang = useSelector((state: RootState) => state.language.changeLanguage);
-    
-    useEffect(() => {
-        dispatch(fetchData());
-    }, [dispatch]);
 
     return(
         <div className={classes.container}>
@@ -23,15 +20,24 @@ const AboutMe: React.FC = () => {
             <img className={classes.img} src={separator} alt="Separador de página" />
             <div className={classes.subtitlesContainer}>
                 <div className={classes.subContainer}>
-                    <h2 className={classes.developmentH2}>{data[lang]?.development[0]}</h2>
+                    <div className={classes.titleContainer}>
+                        <img className={classes.titlePics} src={developmentPic} alt="imagem alusiva a criação de app"/>
+                        <h2 className={classes.developmentH2}>{data[lang]?.development[0]}</h2>
+                    </div>
                     <p className={classes.paragraph}>{data[lang]?.development[1]}</p>
                 </div>
                 <div className={classes.subContainer}>
-                    <h2 className={classes.maintenanceH2}>{data[lang]?.maintenance[0]}</h2>
+                    <div className={classes.titleContainer}>
+                        <img className={classes.titlePics} src={maintenancePic} alt="imagem alusiva a manutenção"/>
+                        <h2 className={classes.maintenanceH2}>{data[lang]?.maintenance[0]}</h2>
+                    </div>
                     <p className={classes.paragraph}>{data[lang]?.maintenance[1]}</p>
                 </div>
                 <div className={classes.subContainer}>
-                    <h2 className={classes.communicationH2}>{data[lang]?.communication[0]}</h2>
+                    <div className={classes.titleContainer}>
+                        <img className={classes.titlePics} src={communicationPic} alt="imagem alusiva a comunicação"/>
+                        <h2 className={classes.communicationH2}>{data[lang]?.communication[0]}</h2>
+                    </div>
                     <p className={classes.paragraph}>{data[lang]?.communication[1]}</p>
                 </div>
             </div>
