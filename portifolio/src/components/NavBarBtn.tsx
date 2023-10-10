d0da7543c7c9111541945d2c6766207e0df204e1
Lucas Gauto <lukscgauto@gmail.com>
2023-10-10 18:12:39 -0300

    Last update. Links working, bugs fixed.

--- portifolio/src/components/NavBarBtn.tsx
+++ portifolio/src/components/NavBarBtn.tsx
@@ -1,15 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, MouseEventHandler } from "react";
 import classes from '../css/NavBarBtn.module.css';
 
 interface NavBarButtonProps {
     typeBtn: string;
+    goTo: string;
 }
 
-const NavBarBtn: React.FC<NavBarButtonProps> = (props) => {
+const NavBarBtn: React.FC<NavBarButtonProps> = ( { typeBtn, goTo } ) => {
+
+    const handleGoTO: MouseEventHandler<HTMLButtonElement> = ()=> {
+        const targetElement = document.getElementById(goTo);
+        if(targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
 
     return(
         <Fragment>
-            <button className={classes.navBtn}>{props.typeBtn}</button>
+            <button type='button' onClick={handleGoTO} className={classes.navBtn}>{typeBtn}</button>
         </Fragment>
     )
 };
